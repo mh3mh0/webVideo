83baa9563beabd4f80452dda31c5134630d28b54
mh3mh0 <jonxca@icloud.com>
2021-01-09 08:43:31 +0100

    ADDED more pic, JS file, an scrowl over carrousel

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const queue = document.querySelector('.carrousel-container');
+const movies = document.querySelector('.movies');
+
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
