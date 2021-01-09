97b723e276100bb39ba4cfbfd05d06f8b69aded8
mh3mh0 <jonxca@icloud.com>
2021-01-09 09:46:44 +0100

    CHANGED id of arrows on HTML and JS, click on arrows do not work yet

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const queue = document.querySelector('.carrousel-container');
-const movies = document.querySelector('.movies');
+const queue = document.querySelector("carrousel-container");
+const movies = document.querySelector("movie");
 
-const leftArrow = document.getElementById('left-arrow');
-const rightArrow = document.getElementById('right-arrow');
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
 
-// ---- ---- ---- Event Listener to rightArrow. ---- ---- ----//
-rightArrow.addEventListener('click', () => {
+// ?---- ---- ---- Event Listener to rightArrow. ---- ---- ----//
+rightArrow.addEventListener("click", () => {
     queue.scrollLeft += queue.offsetWidth;
 })
