c420d5917bca1655151aff44e58cfafd6def258d
mh3mh0 <jonxca@icloud.com>
2021-01-09 09:07:54 +0100

    ADDED JS code to right-arrow, pending to fix

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ const movies = document.querySelector('.movies');
 
 const leftArrow = document.getElementById('left-arrow');
 const rightArrow = document.getElementById('right-arrow');
+
+// ---- ---- ---- Event Listener to rightArrow. ---- ---- ----//
+rightArrow.addEventListener('click', () => {
+    queue.scrollLeft += queue.offsetWidth;
+})
