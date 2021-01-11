5a22d126ed0aa7f64608b7ef4397cbfc35b71762
mh3mh0 <jonxca@icloud.com>
2021-01-11 20:20:57 +0100

    FIXED carrousel, pointmark

--- js/main.js
+++ js/main.js
@@ -1,10 +1,60 @@
-const queue = document.querySelector("carrousel-container");
-const movies = document.querySelector("movie");
+const queue = document.querySelector('.carrousel-container');
+const movies = document.querySelectorAll('.movie');
 
-const leftArrow = document.getElementById("leftArrow");
-const rightArrow = document.getElementById("rightArrow");
+const leftArrow = document.getElementById('leftArrow');
+const rightArrow = document.getElementById('rightArrow');
 
-// ?---- ---- ---- Event Listener to rightArrow. ---- ---- ----//
-rightArrow.addEventListener("click", () => {
+//? ---- ---- ---- ---- Event Listener for rightArrow ---- ---- ---- ---- 
+rightArrow.addEventListener('click', () => {
     queue.scrollLeft += queue.offsetWidth;
+
+    const indicatorActive = document.querySelector('.pointmark .active');
+    if(indicatorActive.nextSibling){
+        indicatorActive.nextSibling.classList.add('active');
+        indicatorActive.classList.remove('active');
+    }
+});
+
+//? ---- ---- ---- ---- Event Listener for leftArrow ---- ---- ---- ---- 
+leftArrow.addEventListener('click', () => {
+    queue.scrollLeft -= queue.offsetWidth;
+
+    const indicatorActive = document.querySelector('.pointmark .active');
+    if(indicatorActive.previousSibling){
+        indicatorActive.previousSibling.classList.add('active');
+        indicatorActive.classList.remove('active');
+    }
+});
+
+//? ---- ---- ---- ---- Pages ---- ---- ---- ---- 
+const pageNumber = Math.ceil(movies.length / 5);
+for(let i = 0; i < pageNumber; i ++){
+    const indicator = document.createElement('button');
+
+    if(i === 0){
+        indicator.classList.add('active');
+    }
+
+    document.querySelector('.pointmark').appendChild(indicator);
+    indicator.addEventListener('click', (e) => {
+        queue.scrollLeft = i * queue.offsetWidth;
+
+        document.querySelector('.indicator .active').classList.remove('active');
+        e.target.classList.add('active');
+    });
+}
+
+//? ---- ---- ---- ---- Hover ---- ---- ---- ---- 
+movies.forEach((movie) =>{
+    movie.addEventListener('mouseenter', (e) => {
+        const element = e.currentTarget;
+        setTimeout(() =>{
+            movies.forEach(movie => movie.classList.remove('hover'));
+            element.classList.add('hover');
+        }, 200);
+    });
+});
+
+queue.addEventListener('mouseleave', () => {
+    movies.forEach(movie => movie.classList.remove('hover'));
 })
