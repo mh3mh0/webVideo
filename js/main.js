1434b23e83799f2edfe14ef1357ef2b1f2e963ea
mh3mh0 <jonxca@icloud.com>
2021-02-02 19:39:01 +0100

    ADDED button to top

--- js/main.js
+++ js/main.js
@@ -96,26 +96,34 @@ window.onload = function () {
   showHide_Info("infoMovie");
 };
 
-//?------------- Button to Up ----------------------*/
-document.getElementById("button__up").addEventListener("click", scrollUp);
+//?------------- Button to Top ----------------------*/
+mybutton = document.getElementById("button__up");
 
-function scrollUp() {
-  var currentScroll = document.documentElement.scrollTop;
-  if (currentScroll > 0) {
-    //window.requestAnimationFrame(scrollUp);
-    //window.scrollTo(0, currentScroll - currentScroll / 2);
-    window.scrollTo(0, 0);
+mybutton.addEventListener("mouseover", function (event) {
+  mybutton.style.transform = "scale(2)";
+});
+mybutton.addEventListener("mouseleave", function (event) {
+  mybutton.style.transform = "scale(1)";
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    mybutton.style.transform = "scale(1)";
+  } else {
+    mybutton.style.transform = "scale(0)";
   }
 }
 
-/*buttonUp = document.getElementById("button__Up");
-
-window.onscroll = function () {
-  var scroll = document.documentElement.scrollTop;
-  if (scroll > 500) {
-    buttonUp.style.transform = "scale(1)";
-  } else if (scroll < 500) {
-    buttonUp.style.transform = "scale(0)";
-  }
-};
-*/
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
